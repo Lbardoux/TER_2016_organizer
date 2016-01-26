c5e3f302fe012c84f1ef8fd3aea2230c8153c8a2
MTLCRBN <lbardoux@gmail.fr>
2016-01-26 16:37:37 +0100

    dernier commit de la journée, ajout de la partie deadline dans les contraintes possibles

--- organizer/src/modele/contraintes/DateLimite.py
+++ organizer/src/modele/contraintes/DateLimite.py
@@ -1,2 +1,51 @@
 #!/usr/bin/python
 # -*-coding:utf-8 -*
+
+import Contrainte
+
+class DateLimite(Contrainte.Contrainte):
+	"""
+	La classe qui définit une deadline dans l'ordonnancement
+	Par exemple, Tel cours doit se finir avant 18h, ou telle UE avant
+	telle date.
+	
+	@ivar _limite : la limite numérique a ne pas atteindre/dépasser
+	
+	@author : Laurent Bardoux p1108365
+	@version : 1.0
+	"""
+	
+	def __init__(self, limite):
+		"""
+		Le constructeur de cette contrainte.
+		@param self : l'argument implicite.
+		@type limite : un entier.
+		@param limite : la valeur maximale (exclue).
+		@postcondition : L'objet est bien initialisé.
+		"""
+		self._limite = limite
+		
+	#fin __init__
+	
+	
+	def injectionContrainte(self):
+		"""
+		La fonction polymorphique qui injecte une lambda expression
+		correspondant à cette contrainte.
+		@param self: l'argument implicite
+		@return une lambda exprimant cette limite M{pour tout x, x < _limite}
+		"""
+		return lambda x : x < self._limite
+	#fin injectionContrainte
+	
+	
+	def limite(self):
+		"""
+		Un accesseur pour la limite de cette contrainte.
+		@param self: l'argument implicite
+		@return la valeur entière représentant cette L{deadline}
+		"""
+		return self._limite
+	#fin limite
+	
+#fin DateLimite
---
+++ organizer/tests/test_DateLimite.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+import unittest
+import sys
+sys.path.insert(0, "../src")
+
+from src.modele.contraintes.DateLimite import *
+
+class Test_DateLimite(unittest.TestCase):
+	"""
+	La classe qui teste le module DateLimite.py
+	@author : Laurent Bardoux p1108365
+	"""
+	
+	def setUp(self):
+		"""
+		A faire avant chaque test
+		"""
+		self.obj = DateLimite(5)
+	#fin setUp
+		
+	
+	def test_injection(self):
+		"""
+		Teste de la fermeture de la lambda expression
+		"""
+		fonction = self.obj.injectionContrainte()
+		self.assertTrue(fonction(2))
+		self.assertFalse(fonction(5))
+		self.assertFalse(fonction(10))
+	#fin test_injection
+	
+	
+	def test_init_getter(self):
+		"""
+		Test du constructeur
+		"""
+		self.assertEqual(self.obj.limite(), 5)
+	#fin test_init_getter
+
+#fin Test_DateLimite
+
+if __name__ == "__main__":
+	unittest.main()
