7fdc106a370ddc101f9592944fcab715cb2a19fa
MTLCRBN <lbardoux@gmail.fr>
2016-01-26 15:24:55 +0100

    push de pyconstraints et des tests

---
+++ organizer/tests/test_PyConstraint.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+import unittest
+import sys
+sys.path.insert(0, "../src")
+
+from src.pyconstraints_1_0_1.pyconstraints.problem import Problem
+
+class Test_PyConstraint(unittest.TestCase):
+	"""
+	Les tests pour la prise en main de la lib pyConstraint
+	@author: Laurent Bardoux p1108365
+	"""
+	
+	def test_priseEnMain(self):
+		"""
+		Premier essai
+		"""
+		p = Problem()
+		p.add_variable("x", range(4))
+		p.add_variable("y", range(4))
+		p.add_constraint(lambda z : z%2 == 0, ["x"])
+		p.add_constraint(lambda a, b : a != b, ["x", "y"])
+		
+		resultats = p.iter_solutions().next()
+		self.assertTrue(resultats["x"]%2 == 0)
+		self.assertTrue(resultats["x"] != resultats["y"])
+	#fin test_priseEnMain
+	
+	def test_echec(self):
+		"""
+		Que se passe-t-il si les contraintes sont trop restrictives ?
+		"""
+		p = Problem()
+	
+#fin Test_PyConstraint
+
+
+if __name__ == "__main__":
+	unittest.main()
+#fin if
