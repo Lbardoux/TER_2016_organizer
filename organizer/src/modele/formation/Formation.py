d33cce3393705a850957a4148a13c03eefef3307
Emilie LIU <emilieliu@me.com>
2016-01-30 16:48:41 +0100

    structure Ue et Formation

--- organizer/src/modele/formation/Formation.py
+++ organizer/src/modele/formation/Formation.py
@@ -3,14 +3,53 @@
 
 class Formation:
 	"""
-	
+	La classe Cm qui hérite la classe seance
+	@ivar _idFormation : l'identifiant de la formation
+	@ivar _niveau : le niveau de la formation 1->L1 2->L2 3->L3 4->M1 5->M2 6->D1 7->D2 8->D3 0->Autres
+	@ivar _nom : le nom de la formation
+	@ivar _idEnseignant : l'identifiant du responsable de la formation
+	@ivar _listeUe
+	@version : 1.0
+	@author : Liu Zhuying
 	"""
-	def __init__(self, nom = "defaut ", id_enseignant):
+	def __init__(self, idFormation, niveau, nom, id_enseignant):
 		"""
+		Le constructeur de la classe Formation.
+		@param self : L'argument implicite
+		@param idFormation : l'identifiant que cette formation aura
+		@type idFormation : entier naturel non nul
+		@param niveau : le niveau que cette formation aura
+		@type niveau : entier naturel
+		@param nom :  le nom que cette formation aura
+		@type nom : str
+		@param id_enseignant : l'identifiant du responsable de cette formation
+		@type id_enseignant : entier naturel non nul
 		"""
-		self._nom = nom
-		self._id_enseignant = id_enseignant
-	
+		if idFormation > 0:
+			self._idFormation = idFormation
+		else:
+			self._idFormation = 1
+		#fin if
+		
+		if niveau > 0:
+			self._niveau = niveau
+		else:
+			self._niveau = 0
+		#fin if
+		
+		if bool(nom.strip()):
+			self._nom = nom
+		else:
+			self._nom = "Nom Par Defaut"
+		#fin if
+		
+		if id_enseignant > 0:
+			self._idEnseignant = id_enseignant
+		else:
+			self._idEnseignant = 1
+		#fin if
+		
+		self._listeUe = []	
 	#fin __init__
 	
 #fin Formation
--- organizer/src/modele/formation/Ue.py
+++ organizer/src/modele/formation/Ue.py
@@ -1,23 +1,53 @@
 #!/usr/bin/python
 # -*-coding:utf-8 -*
 
-class Ue:
+class Ue(object):
 	"""
+	La classe qui représente une unité d'enseignement.
+	@ivar _code : le code UE, par exemple : MIF18, LIF15
+	@ivar _nom : le nom de UE, par exemple : Base de données avancée
+	@ivar _idEnseignant : l'identifiant du responsable de l'Ue
+	@ivar _nombreInscrit : le nombre d'etudiants inscrits dans cette Ue
+	@ivar _nombreGroupe : le nombre de groupes de cette Ue 
+	@ivar _nombreCm : Le nombre de Cms que contient cette Ue
+	@ivar _nombreTd : Le nombre de Tds que contient cette Ue
+	@ivar _nombreTp : Le nombre de Tps que contient cette Ue
+	@ivar _nombreExamen : Le nombre de Examens que contient cette Ue
+	@ivar _nombreAutre : Le nombre de séances autre que CM TD TP Examen
+	@ivar _listeSeance : La liste des seances que cette Ue contient
+	@version : 1.0
+	@author : Liu Zhuying
 	"""
-	def __init__(self, nom = "defaut ", id_enseignant):
+	def __init__(self, code, nom, id_enseignant):
 		"""
+		Le constructeur de la classe Enseignant.
+		@param self : L'argument implicite
+		@param code : l'identifiant que cet enseignant aura
+		@type code : str
+		@param nom : le nom que cet enseignant aura
+		@type nom : str
+		@param id_enseignant : le prenom que cet enseignant aura
+		@type id_enseignant : entier naturel non nul
 		"""
+		self._code = code
 		self._nom = nom
-		self._id_enseignant = id_enseignant
+		
+		if id_enseignant > 0:
+			self._idEnseignant = id_enseignant
+		else:
+			self._idEnseignant = 1
+		#fin if
+		self._nombreInscrit = 1
+		self._nombreGroupe = 1
 		self._nombreCm = 0
 		self._nombreTd = 0
 		self._nombreTp = 0
 		self._nombreExamen = 0
 		self._nombreAutre = 0
-		self._listeElement = list()
+		self._listeSeance = []
 	#fin __init__
 	
-	
+	"""
 	def setNombreCm(self, nombre):
 		"""
 		"""
@@ -52,14 +82,14 @@ class Ue:
 		self._nombreAutre = nombre	
 	#fin setNombreAutre
 	
-	def ajouterElement(self, element):
+	def ajouterSeance(self, Seance):
 		"""
 		"""
-		if type(element) is Element:
-			self._listeElement.insert(element)
+		#if type(element) is Element:
+		#	self._listeElement.insert(element)
 		#fin if
 		
-	#fin ajouterElement
-		
+	#fin ajouterSeance
+	"""	
 #fin Ue
 	
