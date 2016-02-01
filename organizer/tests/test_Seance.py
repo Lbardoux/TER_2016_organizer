18216bf02298555894ecfc38496cd0feaf7cd511
emiliebiscuits <emilieliu@me.com>
2016-02-01 10:19:52 +0100

    ajout la duree

--- organizer/tests/test_Seance.py
+++ organizer/tests/test_Seance.py
@@ -15,14 +15,16 @@ class Test_Seance(unittest.TestCase):
 		"""
 		Test du constructeur.
 		"""
-		cible = Seance(1, 25, 13, "decrit toi")
+		cible = Seance(1, 25, 8, 13, "decrit toi")
 		self.assertEqual(cible.idSeance, 1)
 		self.assertEqual(cible.idGroupe, 25)
+		self.assertEqual(cible.duree, 8)
 		self.assertEqual(cible.idEnseignant, 13)
 		self.assertEqual(cible.description, "decrit toi")
-		cible = Seance(-1, 0)
+		cible = Seance(-1, 0, 6)
 		self.assertEqual(cible.idSeance, 1)
 		self.assertEqual(cible.idGroupe, 1)
+		self.assertEqual(cible.duree, 6)
 		self.assertEqual(cible.idEnseignant, 0)
 		self.assertEqual(cible.description, "")
 	#fin test_init_getters
@@ -32,9 +34,13 @@ class Test_Seance(unittest.TestCase):
 		"""
 		Tests des accesseurs
 		"""
-		cible = Seance(5, 5, 7,"rien")
+		cible = Seance(5, 5, 6, 7,"rien")
 		cible.description = "nada"
 		self.assertEqual(cible.description, "nada")
+		cible.duree = -4
+		self.assertEqual(cible.duree, 6)
+		cible.duree = 8
+		self.assertEqual(cible.duree, 8)
 		cible.idEnseignant = -2
 		self.assertEqual(cible.idEnseignant, 7)
 		cible.idEnseignant = 14
